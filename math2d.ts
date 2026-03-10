a048502bcd973b8a702e1cd598d145021f5f1b7a
Julian Garcia <juliangarcia8784@gmail.com>
2026-03-09 21:48:32 -0500

    creating files for math libraries

---
+++ math2d.ts
@@ -0,0 +1,21 @@
+/* Library for general 2d math functions */
+
+// goals
+
+/* 
+
+1. Triangle 
+
+2. Rectangle
+
+3. Circle
+
+*/
+
+
+// Triangle
+
+export function createTriangleInfo()
+{
+    
+}
